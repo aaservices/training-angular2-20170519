d738e55feb0cd9b141e994113f2d2a7833ce1392
nikodhmo <nikodhmo@hotmail.com>
2017-05-19 16:34:41 +0300

    nmds 2 ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello {{name}}</h1>`,
+    templateUrl: 'app/app.component.html',
 })
-export class AppComponent { name = 'Nicolas Mavroeidis Angularistas...' +
-                            'Νίκο Μαυροειδή Ανγκουλαρίστα...'; }
+export class AppComponent { /*name = 'Nicolas Mavroeidis...';*/ }
