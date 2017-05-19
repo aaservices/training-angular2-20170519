ddd3daf649d6d509fd0a20e2e74d150eaed9a31f
snakemjk <snakemjk@gmail.com>
2017-05-19 16:17:14 +0300

    changesss

---
+++ src/app/accounts/acccount­list.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'account-list',
+    templateUrl: 'app/accounts/account­list.component.html',
+})
+export class AccountListComponent {  }
