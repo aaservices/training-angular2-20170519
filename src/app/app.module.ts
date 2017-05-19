8abdbbf17bb839737a6213ec0ff9c262d9083513
nikodhmo <nikodhmo@hotmail.com>
2017-05-19 16:52:00 +0300

    nmds 2 ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,6 @@ import { AccountListComponent } from './accounts/accountlist.component';
 @NgModule({
     imports: [BrowserModule],
     declarations: [AppComponent, AccountListComponent],
-    bootstrap: [AppComponent, AccountListComponent]
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
