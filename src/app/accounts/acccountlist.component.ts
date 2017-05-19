98b159c5e656a5adda9094ccbe60bd04e13e38ed
LampeaP <LampeaP@piraeusbank.gr>
2017-05-19 06:54:34 -0700

    lampeapa

---
+++ src/app/accounts/acccountlist.component.ts
@@ -0,0 +1,7 @@
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'accountlist',
+    templateUrl: 'app/accounts/accountlist.component.html',
+})
+export class AccountListComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { AccountListComponent } from './accounts/acccountlist.component';
+
+
+
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    declarations: [AppComponent,AccountListComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
