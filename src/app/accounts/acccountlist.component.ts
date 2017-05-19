b855538d2d6c50bf7402dbf778631af79ec73f7d
TiganisK <TiganisK@piraeusbank.gr>
2017-05-19 16:22:13 +0300

    test

---
+++ src/app/accounts/acccountlist.component.ts
@@ -0,0 +1,10 @@
+
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app-accounts',
+    templateUrl: 'app/accounts/accountlist.component.html'
+})
+
+export class AccountListComponent {  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import{ AccountListComponent } from './accounts/acccountlist.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
-    bootstrap: [AppComponent]
+    declarations: [AppComponent,AccountListComponent],
+    bootstrap: [AppComponent,AccountListComponent]
 })
 export class AppModule { }
