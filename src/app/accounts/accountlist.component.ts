d45036523419a718cea276a933ab814d9fe7031e
niarchosd <niarchosd@piraeusbank.gr>
2017-05-22 14:45:41 +0300

    save

---
+++ src/app/accounts/accountlist.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'accountlist',
+    templateUrl: 'app/accounts/accountlist.component.html',
+    //template: `aaaa`,
+})
+
+export class AccountListComponent {  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccountListComponent } from './accounts/accountlist.component';
 
 @NgModule({
     imports: [BrowserModule],
-    declarations: [AppComponent],
+    //declarations: [AppComponent],
+    declarations: [AppComponent, AccountListComponent],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
