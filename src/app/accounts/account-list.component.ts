3a3c7a6ca3a4373128fe66a93d405760f2b597b5
nmds <nmds@nikodhmos.nmdshome.site>
2017-05-28 13:38:23 +0300

    nmds ok

--- src/app/accounts/account-list.component.ts
+++ src/app/accounts/account-list.component.ts
@@ -6,8 +6,13 @@ import {Component} from '@angular/core';
 })
 export class AccountListComponent {
   private listVisibility: boolean;
+  private accounts: Array<{id:string, type:string, amount:number, currency:string}>;
   constructor(){
     this.listVisibility = true;
+    this.accounts=[{id:'100-505-854-1234-001',type:'Current account',amount:5785.55,currency:'EUR'},
+                   {id:'100-505-854-1234-002',type:'Current account',amount:1567.00,currency:'USD'},
+                   {id:'100-505-854-1234-003',type:'Savings account',amount:3670.64,currency:'GBP'},
+                   {id:'100-505-854-1234-004',type:'Savings account',amount:1230.00,currency:'EUR'}];  
   }
   toggleList():void{
     this.listVisibility =!this.listVisibility;
