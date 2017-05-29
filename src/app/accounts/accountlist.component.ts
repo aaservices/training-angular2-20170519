985d022e47ce055defa1ab563ea11c23e2fcda0a
nikodhmo <nikodhmo@hotmail.com>
2017-05-29 15:55:01 +0300

    nmds ok

--- src/app/accounts/accountlist.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'accounts',
-    templateUrl: 'app/accounts/accountlist.component.html',
-})
-export class AccountListComponent { /*name = 'Nicolas Mavroeidis...';*/ }
