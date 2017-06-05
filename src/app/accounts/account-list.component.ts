e79cb7113964ca73412f5cfe913f1c2d51481e97
nmds <nmds@nikodhmos.nmdshome.site>
2017-06-05 13:06:09 +0300

    nmds ok

--- src/app/accounts/account-list.component.ts
+++ src/app/accounts/account-list.component.ts
@@ -20,7 +20,8 @@ export class AccountListComponent {
     this.accounts=[new Account('100-505-854-1234-001','Τρεχούμενος',5785.55,'EUR'),
                    new Account('100-505-854-1234-002','Τρεχούμενος',1567.00,'USD'),
                    new Account('100-505-854-1234-003','Αποταμιευτικός',3670.64,'GBP'),
-                   new Account('100-505-854-1234-004','Αποταμιευτικός',1230.00,'EUR')];  
+                   new Account('100-505-854-1234-004','Αποταμιευτικός',1230.00,'EUR'),
+                   new Account('100-505-854-1234-005','Δανειακός',-2567.15,'EUR')];  
     this.viewAccounts=this.accounts;
   }
   private toggleList():void{
