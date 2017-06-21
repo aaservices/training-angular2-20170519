3e908fb858f0a22d9216935b7b27269471a80ccd
llkoun <leandros.kounadis@camelotglobal.com>
2017-06-21 11:59:58 +0300

    part 1

---
+++ src/app/Forms/reactive-form/form.component.ts
@@ -0,0 +1,41 @@
+import {Component} from "@angular/core";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+    selector: 'reactive-form',
+    templateUrl: './form.component.html'
+})
+
+
+//use of FormControl class only
+export class ReactiveFormComponent {
+    name = new FormControl();
+}
+
+//use of FormGroup along with FormControl
+// export class ReactiveFormComponent {
+//     myForm = new FormGroup ({
+//         name: new FormControl()
+//     });
+// }
+
+//FormBuilder which takes a FormControls in a key value pair paradigm
+// export class ReactiveFormComponent {
+//     myForm: FormGroup; // <--- heroForm is of type FormGroup
+//
+//     constructor(private formBuilder: FormBuilder) { // <--- inject FormBuilder
+//         this.createForm();
+//     }
+//
+//     createForm() {
+//         this.myForm = this.formBuilder.group({
+//             name: '', // <--- the FormControl called "name"
+//         });
+//     }
+// }
+
+
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { Logger } from './logger.service';
 import { AppRoutingModule, routableComponents, routableServices } from './app-routing.module';
 import {TemplateDrivenFormComponent} from "./Forms/template-driven-form/form";
+import {ReactiveFormComponent} from "./Forms/reactive-form/form.component";
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        AppRoutingModule
+        AppRoutingModule,
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent,
         TemplateDrivenFormComponent,
+        ReactiveFormComponent,
         routableComponents
     ],
     bootstrap: [AppComponent],
